32c0d3ee5e524e84910e8dcdf8cdd5950481cb2c
abhinav kannojia <abhinavkannojia@10-18-64-139.dynapool.wireless.nyu.edu>
2020-03-02 19:15:41 -0500

    new

--- Readme.js
+++ Readme.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
 
-var readme = fs.readFileSync('abhi.txt', 'utf8');
-console.log(readme);
+//var readme = fs.readFileSync('abhi.txt', 'utf8');
+fs.readFile('abhi.txt', 'utf8', function {
+
+        }
+        fs.writeFileSync('write.txt', readme);
